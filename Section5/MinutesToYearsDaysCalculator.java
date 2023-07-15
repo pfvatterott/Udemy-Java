c0fd45b2b982b9cd31bbcc1e24a00a23568b8eb2
Paul Vatterott <pfvatterott@gmail.com>
2023-07-15 10:17:09 -0600

    Create MinutesToYearsDaysCalculator.java

---
+++ Section5/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,32 @@
+package Section5;
+
+public class MinutesToYearsDaysCalculator {
+    public static void main(String[] args) {
+        printYearsAndDays(561600);
+    }
+
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        }
+        int hours = 0;
+        if (minutes >= 60) {
+            int remainingMinutes = ((int) minutes) % 60;
+            hours = (((int) minutes) - remainingMinutes) / 60;
+        }
+        int days = 0;
+        if (hours >= 24) {
+            int remainingHours = hours % 24;
+            days = (hours - remainingHours) / 24;
+        }
+        int years = 0;
+        if (days >= 365) {
+            int remainingDays = days % 365;
+            years = (days - remainingDays) / 365;
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+        }
+        else {
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
+}
