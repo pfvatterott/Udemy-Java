bf8aea1a8088b41a53efaa96c9c513c0be804a49
Paul Vatterott <pfvatterott@gmail.com>
2023-07-15 19:02:26 -0600

    Create TraditionalSwitchChallenge.java

---
+++ Section6/TraditionalSwitchChallenge.java
@@ -0,0 +1,29 @@
+package Section6;
+
+public class TraditionalSwitchChallenge {
+    public static void main(String[] args) {
+        int letter = 'H';
+
+        switch (letter) {
+            case 'A':
+                System.out.println("Able");
+                break;
+            case 'B':
+                System.out.println("Baker");
+                break;
+            case 'C':
+                System.out.println("Charlie");
+                break;
+            case 'D':
+                System.out.println("Dog");
+                break;
+            case 'E':
+                System.out.println("Easy");
+                break;
+            default:
+                System.out.println(letter + " not found.");
+                break;
+        }
+    }
+
+}
