c24dc11290c42a178d05188273cdfa52d1884a7a
Paul Vatterott <pfvatterott@gmail.com>
2023-07-20 18:34:01 -0600

    Create LastDigitChecker.java

---
+++ Section6/LastDigitChecker.java
@@ -0,0 +1,22 @@
+package Section6;
+
+public class LastDigitChecker {
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean hasSameLastDigit(int num1, int num2, int num3) {
+        if (num1 < 10 || num1 > 1000 || num2 < 10 || num2 > 1000 || num3 < 10 || num3 > 1000) {
+            return false;
+        }
+        int num1Last = num1 % 10;
+        int num2Last = num2 % 10;
+        int num3Last = num3 % 10;
+        if (num1Last == num2Last || num1Last == num3Last || num2Last == num3Last) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
