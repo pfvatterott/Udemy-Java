54c0aabf7d28bfdfe1bf98c7f2045298cba7122f
Paul Vatterott <pfvatterott@gmail.com>
2023-07-20 19:51:02 -0600

    Create PerfectNumber.java

---
+++ Section6/PerfectNumber.java
@@ -0,0 +1,25 @@
+package Section6;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == number) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
