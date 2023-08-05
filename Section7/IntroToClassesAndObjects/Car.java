020bf3b6da10311fc910718ea3c38e8e1e7743a5
Paul Vatterott <pfvatterott@gmail.com>
2023-08-05 08:32:55 -0600

    folder

---
+++ Section7/IntroToClassesAndObjects/Car.java
@@ -0,0 +1,13 @@
+package Section7.IntroToClassesAndObjects;
+
+public class Car {
+    private String make;
+    private String model;
+    private String color;
+    private int doors;
+    private boolean convertible;
+
+    public void describeCar() {
+        System.out.println(doors + "-Door " + color + " " + make + " " + model + " " + (convertible ? "Convertible" : ""));
+    }
+}
