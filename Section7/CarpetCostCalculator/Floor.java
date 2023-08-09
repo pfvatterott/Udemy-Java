ad4aa2d7b7061d9808ef454b9bea9fda00eb0bbb
Paul Vatterott <pfvatterott@gmail.com>
2023-08-09 07:36:22 -0600

    carpet calculator

---
+++ Section7/CarpetCostCalculator/Floor.java
@@ -0,0 +1,26 @@
+package Section7.CarpetCostCalculator;
+
+public class Floor {
+    
+    private double length;
+    private double width;
+
+    public Floor(double length, double width) {
+        if (width < 0) {
+            this.width = 0;
+        }
+        else {
+            this.width = width;
+        }
+        if (length < 0) {
+            this.length = 0;
+        }
+        else {
+            this.length = length;
+        }
+    }
+
+    public double getArea() {
+        return this.width * this.length;
+    }
+}
---
+++ Section7/CarpetCostCalculator/Main.java
@@ -0,0 +1,7 @@
+package Section7.CarpetCostCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
