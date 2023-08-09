515e5aef4855bd03c5036fcaa98226289223a674
Paul Vatterott <pfvatterott@gmail.com>
2023-08-08 19:38:59 -0600

    Update Student.java

--- Section7/ThePojo/Student.java
+++ Section7/ThePojo/Student.java
@@ -19,4 +19,36 @@ public class Student {
         return "Student [id=" + id + ", name=" + name + ", dateOfBirth=" + dateOfBirth + ", classList=" + classList
                 + "]";
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getClassList() {
+        return classList;
+    }
+
+    public void setClassList(String classList) {
+        this.classList = classList;
+    }
 }
