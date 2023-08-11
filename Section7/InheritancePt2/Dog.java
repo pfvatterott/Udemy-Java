e1b89934b35c03e9b98a916d03c66967774a70b3
Paul Vatterott <pfvatterott@gmail.com>
2023-08-10 21:06:27 -0600

    inheritance pt 2

---
+++ Section7/InheritancePt2/Dog.java
@@ -0,0 +1,35 @@
+package Section7.InheritancePt2;
+
+public class Dog extends Animal {
+
+    private String earShape;
+    private String tailShape;
+    
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+    public Dog(String type, double weight) {
+        this(type, weight, "Perky", "Curled");
+    }
+
+    public Dog() {
+        super("Mutt", "Big", 50);
+    }
+
+    @Override
+    public String toString() {
+        return "Dog [earShape=" + earShape + ", tailShape=" + tailShape + "] " + super.toString();
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+        System.out.println("Dogs walk, run and wag their tail");
+    }
+
+    
+    
+}
